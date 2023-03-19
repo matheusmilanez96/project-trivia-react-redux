6a39dffe93540855218715a7d19d83dfa2aa0303
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-03-18 23:18:07 -0300

    Requisito 8: Concluído

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import Question from '../components/Question';
-import Timer from '../components/Timer';
 // import PropTypes from 'prop-types';
 
 class Game extends Component {
   render() {
     return (
       <div>
-        <Timer />
         <Question />
       </div>
     );
