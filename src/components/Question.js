4bf14e46e814a78cfd747faabeaefa914d982a44
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-03-18 22:37:05 -0300

    Requisito 8: Cria uma classe timer que define um timer e mostra na tela de jogo o tempo que o jogador tem pra responder a pergunta

--- src/components/Question.js
+++ src/components/Question.js
@@ -4,39 +4,56 @@ import PropTypes from 'prop-types';
 import { Redirect } from 'react-router-dom';
 import { getQuestions } from '../redux/actions/gameActions';
 import Answers from './Answers';
+import Timer from '../helpers/timer';
+
+const delay = 1000;
 
 class Question extends Component {
   state = {
-    actualQuestion: 0,
+    actualQuestionIndex: 0,
+    time: 30,
+    // actualQuestion: {},
   };
+
+  timer = new Timer(() => {
+    const { time } = this.state;
+    if (time === 0) this.timer.pause();
+    else this.setState({ time: time - 1 });
+  }, delay);
 
   componentDidMount() {
     const { dispatch } = this.props;
     dispatch(getQuestions());
+    this.timer.resume();
   }
 
   render() {
     const { questions, responseCode } = this.props;
-    const { actualQuestion } = this.state;
+    const { actualQuestionIndex, time } = this.state;
     const invalidTokenResponse = 3;
 
     if (responseCode === invalidTokenResponse) return <Redirect to="/" />;
-
     if (questions.length === 0) return <p>Loading...</p>;
 
     const {
       category,
       question,
-      correct_answer: correct,
-      incorrect_answers: incorrects,
-    } = questions[actualQuestion];
+    } = questions[actualQuestionIndex];
+
     return (
       <div>
         <p data-testid="question-category">{ category }</p>
         <div>
           <p data-testid="question-text">{ question }</p>
         </div>
-        <Answers answers={ { correct, incorrects } } />
+        <Answers
+          answers={ questions[actualQuestionIndex] }
+          remaning={ time }
+          stopTimer={ () => this.timer.pause() }
+        />
+        <div>
+          { `Timer: ${time}` }
+        </div>
       </div>
     );
   }
