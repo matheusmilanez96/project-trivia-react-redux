8cff001bbecc29b33201b10055e48786c0790519
Caroline <carolinefernandesc@hotmail.com>
2023-03-20 11:01:49 -0300

    Requisito 12: Iniciando

---
+++ src/pages/Feedback.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Header from '../components/Header';
+// import Score from '../components/Score';
+
+class Feedback extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default Feedback;
