d3d7e845dbdeb757fa209d475a0e5a2e7ee319aa
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-03-20 16:16:42 -0300

    Requisito 11: Concluído

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,22 +1,24 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Question from '../components/Question';
-// import Score from '../components/Score';
 import Header from '../components/Header';
-// import PropTypes from 'prop-types';
 
 class Game extends Component {
   render() {
+    const { history } = this.props;
     return (
       <div>
         <Header />
-        <Question />
+        <Question history={ history } />
       </div>
     );
   }
 }
 
 Game.propTypes = {
-
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
 };
 
 export default Game;
