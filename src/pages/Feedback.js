8593c17412e2c067acd151b8856b031041e847e1
Thiago <tm.thiagomorais@gmail.com>
2023-03-20 15:20:49 -0300

    req 16

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 // import Score from '../components/Score';
 
 class Feedback extends Component {
+  constructor() {
+    super();
+    this.rankingClick = this.rankingClick.bind(this);
+  }
+
+  rankingClick = () => {
+    const { history } = this.props;
+    history.push('/ranking');
+  };
+
   render() {
     return (
       <div>
         <Header />
+        <button
+          type="button"
+          data-testid="btn-ranking"
+          onClick={ this.rankingClick }
+        >
+          Ranking
+        </button>
       </div>
     );
   }
 }
+
+Feedback.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Feedback;
