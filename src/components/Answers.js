69dc30e33398b6041946758c6ec1e883d52c6c3d
Caroline <carolinefernandesc@hotmail.com>
2023-03-20 18:08:10 -0300

    Requisito 13: Concluído

--- src/components/Answers.js
+++ src/components/Answers.js
@@ -71,7 +71,6 @@ class Answers extends Component {
       dispatch(increaseScore({
         remaining,
         difficulty: difficultyPoints[questionDifficulty],
-        assertions: 1,
       }));
     }
   };
